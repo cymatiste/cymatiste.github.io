faabc6fe3a664cc1ff38ea183a94d8b98ae34988
Sarah Imrisek <sarahimrisek@gmail.com>
2025-09-24 12:57:36 -0400

    code for hydra

---
+++ hydramedia/hexes.js
@@ -0,0 +1,27 @@
+hexes =     [
+	    "#6082b6", //00_Glaucous
+	    "#E48400", //01_Fulvous
+	    "#16161D", //02_Eigengrau
+	    "#40826D", //03_Viridian
+	    "#FFC40C", //04_MikadoYellow
+	    "#0099EE", //05_Pervenche
+	    "#DF00FF", //08_Phlox
+	    "#E52B50", //09_Amaranth
+	    "#3AB09E", //10_Keppel
+	    "#BC3F4A", //11_Sanguine
+	    "#E3F988", //13_Mindaro
+	    "#43B3AE", //15_Verdigris
+	    "#F28500", //16_Tangerine
+	    "#E63E62", //20_ParadisePink
+	    "#273C76", //22_Mazarine
+	    "#8E3A59", //23_Quinacridone
+	    "#00A86B", //24_Jade
+	    "#AA0022", //25_Incarnadine
+	    "#ACE1AF", //28_Celadon
+	    "#DF73FF", //30_Heliotrope
+	    "#e6be51", //31_Opriment
+	    "#1f6be5", //32_Verditer
+	    "#B7410E", //34_Rust
+	    "#7FDD4C", //35_Absinthe
+	    "#FCF75E" //37_Icterine
+	  ];
